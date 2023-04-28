354184f316c005b87dbc5b2ce129aeb3c76a490f
dsl2022 <2015rpro@gmail.com>
2023-04-27 20:01:25 -0400

    first commit

---
+++ spread-rest-opt.js
@@ -0,0 +1,19 @@
+// Write a function that takes two arrays and returns a new array that contains all the elements of both arrays, using the spread operator.
+
+function concatArrays(arr1, arr2){
+    // TODO, write your code here
+}
+
+console.log(concatArrays([1, 2, 3], [4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
+console.log(concatArrays(['apple', 'banana'], ['cherry', 'dragonfruit'])); // Output: ['apple', 'banana', 'cherry', 'dragonfruit']
+
+
+// Write a function that takes an array and returns a new array with the first and last elements removed, using the rest operator.
+
+function removeFirstAndLast(){
+
+}
+
+console.log(removeFirstAndLast([1, 2, 3, 4])); // Output: [2, 3]
+console.log(removeFirstAndLast(['apple', 'banana', 'cherry', 'dragonfruit'])); // Output: ['banana', 'cherry']
+
